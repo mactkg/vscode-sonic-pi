19f33a96bb03b3298c12b38983fdd3d7f282e1f1
mogimogitomato <osakana.yano49@gmail.com>
2020-01-05 12:17:27 +0900

    lightweight refactoring

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,14 @@
 import * as vscode from 'vscode';
 
-const BACKGROUND_COLOR_FLASHED = 'rgba(255,20,147,1.0)'
-const TEXT_COLOR_FLASHED = 'rgba(255,255,255,1.0)'
+const BACKGROUND_COLOR_FLASHED = 'rgba(255,20,147,1.0)';
+const TEXT_COLOR_FLASHED = 'rgba(255,255,255,1.0)';
 
 export class Config {
     private getConfiguration = vscode.workspace.getConfiguration;
     private section: string = 'sonicpi';
 
     public flashBackgroundColor(): string {
-        return this.getConfiguration(this.section).get('flashBackgroundColor', BACKGROND_COLOR_FLASHED);
+        return this.getConfiguration(this.section).get('flashBackgroundColor', BACKGROUND_COLOR_FLASHED);
     }
     public flashTextColor(): string {
         return this.getConfiguration(this.section).get('flashTextColor', TEXT_COLOR_FLASHED);
