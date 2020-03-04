fe70308570f9a0eef8ec619b88372d6023b60dd8
YK <osakana.yano49@gmail.com>
2020-03-05 02:16:51 +0900

    get osc's port from SP's boot log(Fix #3)

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,13 @@
 'use strict';
-import { window, commands, ExtensionContext, Range, TextEditor } from 'vscode';
+import { workspace, window, commands, ExtensionContext, Range, TextEditor } from 'vscode';
 import { Config } from './config';
 import * as OSC from 'osc-js';
 import * as ICONV from 'iconv-lite';
+import * as HOME_DIR from 'user-home';
 
-export function activate(context: ExtensionContext) {
+export async function activate(context: ExtensionContext) {
     let sonicPi = new SonicPi();
+    await sonicPi.initOSC();
 
     let runCodeCmd = commands.registerCommand('extension.runCode', () => {
         sonicPi.runCode();
@@ -21,7 +23,6 @@ export function activate(context: ExtensionContext) {
             sonicPi.runCode();
         }, 500);
     });
-
     context.subscriptions.push(runCodeCmd);
     context.subscriptions.push(stopAllCmd);
     context.subscriptions.push(restartCmd);
@@ -40,10 +41,6 @@ class SonicPi {
 
     constructor() {
         this.config = new Config();
-        this.osc = new OSC({
-            plugin: new OSC.DatagramPlugin({ send: { port: 4557 } })
-        });
-        this.osc.open({ port: 4558 });
     }
 
     public runCode() {
@@ -68,6 +65,15 @@ class SonicPi {
     public getCurrentCode(editor: TextEditor): Buffer | undefined {
         return ICONV.encode(editor.document.getText(), "utf-8");
     }
+
+    public async initOSC() {
+        const ports = await this.getDynamicPort();
+        this.osc = new OSC({
+            plugin: new OSC.DatagramPlugin({ send: { port: ports.server_listen_port } })
+        });
+        this.osc.open({ port: ports.gui_listen_port });
+    }
+
     private flashCode(editor: TextEditor) {
         let startPos = editor.document.positionAt(0);
         let endPos = editor.document.positionAt(editor.document.getText().length - 1);
@@ -81,6 +87,30 @@ class SonicPi {
             flashDecorationType.dispose();
         }, 250);
     }
+
+    private async getDynamicPort() {
+        let ports = { server_listen_port: 4557, gui_listen_port: 4558 };
+        const doc = await workspace.openTextDocument(HOME_DIR + '/.sonic-pi/log/gui.log');
+        if (!doc) { return ports; }
+        const text = doc.getText();
+        const server_regex = /Server listen to gui port\s+(\d+)/;
+        const server_listen_port = this.extractPortNumber(text, server_regex);
+        const gui_regex = /GUI listen to server port\s+(\d+)/;
+        const gui_listen_port = this.extractPortNumber(text, gui_regex);
+        // both port numbers are always output in boot log.
+        if (server_listen_port && gui_listen_port) {
+            ports.server_listen_port = server_listen_port;
+            ports.gui_listen_port = gui_listen_port;
+        }
+        return ports;
+    }
+
+    private extractPortNumber(text: string, regex: RegExp) {
+        const arr = regex.exec(text);
+        const port = arr ? arr[1] : '';
+        return Number(port);
+    }
+
     dispose() {
         this.osc.close();
     }
